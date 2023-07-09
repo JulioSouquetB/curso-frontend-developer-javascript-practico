ee87a5ec6612e1915f578ed4b08630670d86b58e
JulioSouquetB <124322663+JulioSouquetB@users.noreply.github.com>
2023-07-09 18:01:14 -0400

    Lista de aplicacion JS

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
-const aside = document.querySelector('.product-detail')
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer')
 const cardsContainer = document.querySelector('.cards-container')
 
 
@@ -12,19 +12,19 @@ menuHamIcon.addEventListener('click', toggleMobileMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu(){
-  const isAsideClosed = aside.classList.contains('inactive');
+  const isshoppingCartContainerClosed = shoppingCartContainer.classList.contains('inactive');
 
-  if(!isAsideClosed){
-    aside.classList.add('inactive');
+  if(!isshoppingCartContainerClosed){
+    shoppingCartContainer.classList.add('inactive');
   }
 
   desktopMenu.classList.toggle('inactive');
 }
 function toggleMobileMenu(){
-  const isAsideClosed = aside.classList.contains('inactive');
+  const isshoppingCartContainerClosed = shoppingCartContainer.classList.contains('inactive');
 
-  if(!isAsideClosed){
-    aside.classList.add('inactive');
+  if(!isshoppingCartContainerClosed){
+    shoppingCartContainer.classList.add('inactive');
   }
 
   mobileMenu.classList.toggle('inactive');
@@ -35,7 +35,7 @@ function toggleCarritoAside(){
   if(!isMobileMenuClosed){
       mobileMenu.classList.add('inactive');
     }
-    aside.classList.toggle('inactive');
+    shoppingCartContainer.classList.toggle('inactive');
 
 }
 
@@ -55,21 +55,7 @@ productList.push({
   price: 320,
   image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
 })
-productList.push({
-  name:'Bike',
-  price: 120,
-  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-})
-productList.push({
-  name:'Pantalla',
-  price: 220,
-  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-})
-productList.push({
-  name:'Computador',
-  price: 320,
-  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-})
+
 
 function renderProducts(arr){
 
